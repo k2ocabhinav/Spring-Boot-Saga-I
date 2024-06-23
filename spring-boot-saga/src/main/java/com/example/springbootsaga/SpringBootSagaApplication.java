93f4c285c93c8d56a3e1e3a36989ffa95ed5526b
Abhinav Prasad <k2ocabhinav22@gmail.com>
2024-06-23 12:08:30 +0530

    Set up Spring Boot project with basic metadata

---
+++ spring-boot-saga/src/main/java/com/example/springbootsaga/SpringBootSagaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSagaApplication.class, args);
+	}
+
+}
---
+++ spring-boot-saga/src/test/java/com/example/springbootsaga/SpringBootSagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
