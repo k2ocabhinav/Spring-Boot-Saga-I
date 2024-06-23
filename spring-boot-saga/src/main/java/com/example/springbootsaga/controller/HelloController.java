f71e24e99e0a011c3d8dad20ad63bf3f1850d846
Abhinav Prasad <k2ocabhinav22@gmail.com>
2024-06-23 13:42:58 +0530

    Add GET mapping

--- spring-boot-saga/src/main/java/com/example/springbootsaga/controller/HelloController.java
+++ spring-boot-saga/src/main/java/com/example/springbootsaga/controller/HelloController.java
@@ -1,13 +1,13 @@
 package com.example.springbootsaga.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String helloworld(){
         return "Hi, welcome to the Spring Boot Saga!";
     }
