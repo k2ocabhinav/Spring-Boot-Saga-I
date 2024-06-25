198191c50b33aed0c7941807b49a5d86a8c43fb1
Abhinav Prasad <k2ocabhinav22@gmail.com>
2024-06-25 22:41:43 +0530

    Add global exception handler

---
+++ spring-boot-saga/src/main/java/com/example/springbootsaga/error/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.springbootsaga.error;
+
+import com.example.springbootsaga.entity.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice //Global error handler
+@ResponseStatus
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(DepartmentNotFoundException.class) //Invokes when DNFE gets thrown anywhere in code
+    public ResponseEntity<ErrorMessage> departmentNotFoundException(DepartmentNotFoundException exception,
+                                                                   WebRequest request) {
+        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+    }
+}
