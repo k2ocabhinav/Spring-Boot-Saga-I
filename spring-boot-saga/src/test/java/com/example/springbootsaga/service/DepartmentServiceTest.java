1835005a1b60926f033b43f3a83c94a1c94f3b14
Abhinav Prasad <k2ocabhinav22@gmail.com>
2024-06-26 19:58:38 +0530

    Service layer test

---
+++ spring-boot-saga/src/test/java/com/example/springbootsaga/service/DepartmentServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.springbootsaga.service;
+
+import com.example.springbootsaga.entity.Department;
+import com.example.springbootsaga.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    void setUp() {
+        Department department = Department.builder()
+                .departmentName("IT")
+                .departmentAddress("ADI")
+                .departmentCode("IT-01")
+                .departmentId(1L)
+                .build();
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT"))
+                .thenReturn(department);
+    }
+
+    @Test
+    public void whenValidDepartmentName_thenDepartmentShouldFound(){
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByName(departmentName);
+
+        assertEquals(found.getDepartmentName(), departmentName);
+    }
+}
